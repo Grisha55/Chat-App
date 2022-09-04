4f7d9bdd1170a61d6f38fddb7df4c0c02aa16855
Gregory <grishavinyar@icloud.com>
2022-09-04 17:23:43 +0300

    [init] project

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Григорий Виняр on 04/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .white
+  }
+
+
+}
+
