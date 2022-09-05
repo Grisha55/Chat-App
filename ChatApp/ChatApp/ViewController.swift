76ac6e48a6016dd12ea4840408cafd63561080aa
Gregory <grishavinyar@icloud.com>
2022-09-05 09:34:29 +0300

    [setup] UI for ViewController

--- ChatApp/ChatApp/ViewController.swift
+++ ChatApp/ChatApp/ViewController.swift
@@ -8,13 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  // MARK: - Properties
+  private let usersTableView: UITableView = {
+    let table = UITableView()
+    table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    return table
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     
     view.backgroundColor = .white
+    
+    setupUsersTableView()
   }
 
+  // MARK: - Methods
+  private func setupUsersTableView() {
+    view.addSubview(usersTableView)
+    usersTableView.delegate = self
+    usersTableView.dataSource = self
+    
+    usersTableView.translatesAutoresizingMaskIntoConstraints = false
+    usersTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    usersTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    usersTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    usersTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+  }
 
 }
 
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    
+    // TODO: - Transition to ChatViewController
+  }
+  
+}
+
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = "John Smith"
+    cell.accessoryType = .disclosureIndicator
+    return cell
+  }
+  
+}
